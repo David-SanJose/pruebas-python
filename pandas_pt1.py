0fa18429d80dff63901e6a2e50dcc54e8f7215f1
David <dsjgdavid@gmail.com>
2021-10-26 23:27:19 +0200

    Union tweets y retweets

--- pandas_pt1.py
+++ pandas_pt1.py
@@ -9,14 +9,27 @@ with open('SpydyNWH.json', 'r') as f:
     tweets_str = f.read()
     tweets_json = json.loads(tweets_str)
 
+    column_names = [
+        "id", "text",
+        "lang", "user.screen_name",
+        "user.description", "user.followers_count",
+        "user.friends_count", "user.favourites_count",
+        "user.statuses_count", "user.created_at",
+        "retweet_count", "favorite_count", "reply_count",
+        "quote_count"]
+    column_names_retw = ["retweeted_status." + s for s in column_names]
+
     df_full = pd.json_normalize(tweets_json)
-    df = pd.DataFrame(df_full.get(["id", "text",
-                                   "lang", "user.screen_name",
-                                   "user.description", "user.followers_count",
-                                   "user.friends_count", "user.favourites_count",
-                                   "user.statuses_count", "user.created_at",
-                                   "retweet_count", "favorite_count", "reply_count",
-                                   "quote_count"]))
+    # Dataframes de tweets y tweets retweeteados
+    df = pd.DataFrame(df_full.get(column_names))
+    df_retw = pd.DataFrame(df_full.get(column_names_retw))
+    # Obtenemos un diccionario de equivalencias entre nombres de columnas
+    dict_column_equivalences = dict(zip(column_names_retw, column_names))
+    # Renombramos todas las columnas para igualarlas a las de df
+    df_retw.rename(columns= dict_column_equivalences, inplace=True)
+
+    df = df.append(df_retw)
+
     df = df.drop_duplicates()
     df = df.dropna(thresh=len(df.columns) - 2)
     df = df.sample(frac=1)
@@ -29,6 +42,7 @@ with open('SpydyNWH.json', 'r') as f:
     plt2 = sns.barplot(x=lang, y=count)
     plt2.set(xlabel="Idioma", ylabel="Total")
     plt.show()
+
     # Relación de variables
     df_num = df.get(["user.followers_count",
                      "user.friends_count", "user.favourites_count",
